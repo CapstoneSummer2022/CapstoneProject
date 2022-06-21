b9246d76ab130fac36298e4ca5efde69cab9f82e
Your Name <you@example.com>
2022-06-21 14:03:29 +0700

    Create

---
+++ src/main/java/com/example/electriccomponentsshop/ElectricComponentsShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.electriccomponentsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricComponentsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricComponentsShopApplication.class, args);
+    }
+
+}
