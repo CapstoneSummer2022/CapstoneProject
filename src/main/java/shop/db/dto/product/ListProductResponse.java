259c718eaf84ddbd55a38d342ccd39b59c368005
thuongnthe141559 <ntlu7890@gmail.com>
2022-09-11 01:09:13 +0700

    BE - Get List Product By Category - In Progress

---
+++ src/main/java/shop/db/dto/product/ListProductResponse.java
@@ -0,0 +1,10 @@
+package shop.db.dto.product;
+
+import shop.db.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListProductResponse {
+    List<Product> products = new ArrayList<>();
+}
